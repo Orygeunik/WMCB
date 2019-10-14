1bdade7eccd0a7b1ab680e3b51a98e5817cddc33
Orygeunik <Orygeunik@users.noreply.github.com>
2019-10-14 05:24:17 +0300

    Final (potential) step of adding a modal system
    Добавлена блокировка прокрутки окна "под" модальном окном
    
    Adding blocking the scroll window "under" modal window

--- scripts/Modal/ModalSystem.js
+++ scripts/Modal/ModalSystem.js
@@ -5,12 +5,15 @@
 var modalWindow;
 var modalWindowContentParagraph;
 var modalWindowCloseDummy;
+var mainBodyElement;
 
 function initModalWindowSystem() {
     modalWindow = document.getElementById("descriptionModalWindow");
     modalWindowCloseDummy = document.getElementById("descriptionModalWindowCloseDummy");
 
     modalWindowContentParagraph = document.querySelector(".modalWindowContent p");
+
+    mainBodyElement = document.body;
 
     linkEventTo(modalWindowCloseDummy, "click", closeModalWindow);
     linkEventTo(window, "click", closeModalWindow);
@@ -19,10 +22,12 @@ function initModalWindowSystem() {
 function openModalWindow(textInModalWindow) {
     modalWindowContentParagraph.innerText = textInModalWindow;
     modalWindow.style.display = "block";
+    mainBodyElement.style.overflow = "hidden";
 }
 
 function closeModalWindow() {
     if (event.target === modalWindow || event.target === modalWindowCloseDummy) {
         modalWindow.style.display = "none";
+        mainBodyElement.style.overflow = "visible";
     }
 }
