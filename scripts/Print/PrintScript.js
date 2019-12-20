a3226a6820731d41a3fa1a0663d8d660113e90a9
Orygeunik <Orygeunik@users.noreply.github.com>
2019-12-21 01:32:34 +0300

    Improving connection count and collapsing small scripts
    Уменьшено количество запросов соединений путем уменьшения количества скриптов.
    Малые скрипты соединены в единый
    
    Cut count of network request by cut count of scripts.
    Small scripts merged into union script

--- scripts/Print/PrintScript.js
+++ scripts/Print/PrintScript.js
@@ -18,3 +18,63 @@ function printSetup() {
     window.onbeforeprint = prepareDocument;
     window.onafterprint = revertDocument;
 }
+
+function prepareDocument() {
+
+    let selectsToPrepress = document.getElementsByTagName("SELECT");
+    for (let index = 0; index < selectsToPrepress.length; index++) {
+        removePlaceholder(selectsToPrepress[index]);
+    }
+}
+
+function revertDocument() {
+
+    let selectsToPrepress = document.getElementsByTagName("SELECT");
+    for (let index = 0; index < selectsToPrepress.length; index++) {
+        revertPlaceholder(selectsToPrepress[index]);
+    }
+}
+
+function removePlaceholder(element) {
+
+    if (!element || element.tagName !== "SELECT") {
+        return;
+    }
+
+    if (element.selectedOptions.length === 0) {
+        return;
+    }
+
+    let selectedElement = element.selectedOptions[0];
+
+    if (!selectedElement) {
+        return;
+    }
+
+    if (selectedElement.value === "Empty") {
+        selectedElement.textContent = "";
+    }
+
+}
+
+function revertPlaceholder(element) {
+
+    if (!element || element.tagName !== "SELECT") {
+        return;
+    }
+
+    if (element.selectedOptions.length === 0) {
+        return;
+    }
+
+    let selectedElement = element.selectedOptions[0];
+
+    if (!selectedElement) {
+        return;
+    }
+
+    if (selectedElement.value === "Empty") {
+        selectedElement.textContent = "Пусто";
+    }
+
+}
